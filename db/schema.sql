3353969ba842f0ec26c60757c18fc309378f9b9d
Stroupe79 <cstroupe79@gmail.com>
2019-05-29 09:19:51 -0500

    build out of database complete

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,21 @@
-In the `db` folder, create a file named `schema.sql`. Write SQL queries this file that do the following:
+-- In the `db` folder, create a file named `schema.sql`. Write SQL queries this file that do the following:
 
-   * Create the `tacos_db`.
-   * Switch to or use the `tacos_db`.
-   * Create a `tacos` table with these fields:
-     * **id**: an auto incrementing int that serves as the primary key.
-     * **taco_name**: a string.
-     * **shell**: a string (soft/hard ~ flour/corn)
-     * **vegetarian**: a boolean
-     * **picked_up**: a boolean.
+--    * Create the `tacos_db`.
+--    * Switch to or use the `tacos_db`.
+--    * Create a `tacos` table with these fields:
+--      * **id**: an auto incrementing int that serves as the primary key.
+--      * **taco_name**: a string.
+--      * **shell**: a string (soft/hard ~ flour/corn)
+--      * **vegetarian**: a boolean
+--      * **picked_up**: a boolean.
+
+
+     CREATE DATABASE tacos_db;
+     USE tacos_db;
+     CREATE TABLE tacos (
+         id INT (5) AUTO_INCREMENT PRIMARY KEY,
+         taco_name VARCHAR (10),
+         shell VARCHAR (6),
+         veg BOOLEAN,
+         picked_up BOOLEAN
+     );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,10 @@
-insert queries to populate the `tacos` table with at least three entries.
+-- insert queries to populate the `tacos` table with at least three entries.
+
+
+USE tacos_db;
+INSERT INTO tacos (taco_name, shell, veg, picked_up)
+VALUES 
+("Monster Taco", "hard", false, false),
+("Johnny Taco", "hard", true, false),
+("Loopy Taco", "soft", false, false),
+("Guac Taco", "soft", true, false);
